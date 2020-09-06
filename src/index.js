27bf72dd7b95478458ca37b4aed254fd211ae1a4
Shashwat Sinah <shashwatsinha2702@gmail.com>
2020-09-06 13:00:39 +0530

    Commiting unstaged changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './components/Homepage/HomePage';
 import Routes from './routes';
 import * as serviceWorker from './serviceWorker';
 
